effe2fe57197a08311b6b5cb44ef7eb76ae98952
Jonathan Davito <jonathandavito@BalthasarMagi2.attlocal.net>
2023-09-17 19:14:46 -0400

    fixed js

--- js/main.js
+++ js/main.js
@@ -3,18 +3,25 @@ function expYears(){
     const currentYear = new Date().getFullYear();
     const startYear = 2014;
     const YOE = currentYear - startYear;
-
-    document.getElementById("YOE").textContent = YOE;
+    if (document.getElementById("YOE")) {
+        document.getElementById("YOE").textContent = YOE;
+    } else {
+        return;
+    }
+    
 }
 
 function thanksFonts(){
     var fontType = ["Emeritus", "Taklobo", "Tangley", "Swirl"];
     var num = Math.floor(Math.random()*4);
-    
-    document.getElementById("thanks").style.fontFamily = fontType[num];
+    if (document.getElementById("thanks")) {
+        document.getElementById("thanks").style.fontFamily = fontType[num];
+    } else {
+        return;
+    }
 }
 
 window.addEventListener("load", myInit, true); function myInit(){  
-    //expYears(); 
+    expYears(); 
     thanksFonts();
 };
